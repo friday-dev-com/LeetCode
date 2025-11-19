4b8f2695071bac9285d19a5277d130c2a01169ce
friday-dev-com <princesinghmankotia@gmail.com>
2025-11-19 16:33:09 +0530

    Time: 0 ms (100%), Space: 16.8 MB (59.37%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int n = nums.size();
+    
+        vector<int>ans(2*n);
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            ans[ i ] = nums[i];
+            ans[ i+n ] = nums[i];
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+        
+//         nums.insert( nums.end(), nums.begin(), nums.end() );
+//         return nums;
+//     }
+// };
