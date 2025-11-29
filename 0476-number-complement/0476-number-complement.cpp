c456f19f4918e4ce9a45860bf4bc2c138d902a89
friday-dev-com <princesinghmankotia@gmail.com>
2025-11-29 14:18:37 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.23%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        if ( n == 0 ){
+            return 1;
+        }
+        int val = n ;
+        int mask = 0;
+
+        while ( val != 0) {
+            val = val >> 1;
+            mask = mask<<1 | 1;
+        }
+        cout <<mask;
+        return ~n & mask;
+    }
+};
