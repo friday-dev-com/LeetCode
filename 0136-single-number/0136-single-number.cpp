56ecad7dacd238968fe08ad965971505c83ae6f3
friday-dev-com <princesinghmankotia@gmail.com>
2025-12-01 22:27:38 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.73%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for ( int it : nums ){
+            ans ^= it;
+        }
+        return ans;
+    }
+};
