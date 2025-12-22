71eef95cc693f540f3a2d5947a394c395727c1ec
ketox <princesinghmankotia@gmail.com>
2025-12-22 10:47:00 +0530

    Time: 0 ms (100%), Space: 32.4 MB (61.88%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
+    // god for the large number of test case;
     vector<int> findMissingElements(vector<int>& nums) {
-        unordered_set<int>st;
 
-        int a = INT_MAX;
-        int b = INT_MIN;
+        int n = nums.size();
 
-        for( int i = 0; i < nums.size(); i++ ){
-            int val = nums[i];
-            a = min(a,val);
-            b = max(b,val);
-            st.insert(val);
-        }
+        sort(nums.begin(),nums.end());
         vector<int>ans;
-        for ( int i = a; i <= b; i++ ){
-            if(st.find(i) == st.end()){
-                ans.push_back(i);
+
+        for ( int i = 1 ; i < n; i++){
+            if( nums[i] == nums[i-1]){
+                continue;
+            }
+
+            for( int j = nums[i-1]+1; j < nums[i]; j++){
+                ans.push_back(j);
             }
         }
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         unordered_set<int>st;
+
+//         int a = INT_MAX;
+//         int b = INT_MIN;
+
+//         for( int i = 0; i < nums.size(); i++ ){
+//             int val = nums[i];
+//             a = min(a,val);
+//             b = max(b,val);
+//             st.insert(val);
+//         }
+//         vector<int>ans;
+//         for ( int i = a; i <= b; i++ ){
+//             if(st.find(i) == st.end()){
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+//     }
+// };
